26588a3ca9cef6468929a2bf53350b31b3174a01
FetheZZ <fet98@proton.me>
2026-08-22 17:17:46 +0200

    Ejercicio palabras_comunes terminado.

---
+++ PREWORK-IA/PYTHON/MANEJO DE ARCHIVOS Y EXCEPCIONES/3.palabras_comunes.py
@@ -0,0 +1,18 @@
+'''
+PALABRAS COMUNES
+
+Encuentra o crea algunos textos que te gustaría analizar 
+(puedes visitar Project Gutenberg (http://gutenberg.org/) 
+o crear textos usando ChatGPT). Copia el texto sin formato desde tu navegador 
+en un archivo de texto en tu computadora (o descarga los archivos). 
+Averigua cuántasveces aparece una palabra o frase en el texto 
+(puedes usar el método count()).
+
+'''
+#Abrir el texto.
+with open('archivo_analizar.txt') as archivo:
+    contenido = archivo.read()#Leer el texto
+    num_veces = contenido.count("salpicón")#Contar el numero de veces 
+                                           #que sale una palabra o letra.
+    print(num_veces)
+#La funcion .count() hace todo el trabajo.
